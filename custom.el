552700ad523dda04602cf40661ba7b65323cd4f2
Brian Rowe <brian@ecomedes.com>
2022-02-05 11:11:13 -0500

    store new config & gitignore

--- custom.el
+++ custom.el
@@ -5,13 +5,21 @@
  ;; If there is more than one, they won't work right.
  '(backup-directory-alist '(("." . "~/.emacs-backups")))
  '(delete-old-versions t)
+ '(lsp-rust-analyzer-download-url
+   "https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-aarch64-apple-darwin.gz")
+ '(lsp-rust-server 'rust-analyzer)
  '(safe-local-variable-values
    '((eval progn
+	   (setq after-save-hook
+		 (remove 'my-typescript-formatter after-save-hook)))
+     (cider-lein-parameters . "with-profile +java-8 repl :headless :host localhost")
+     (eval progn
 	   (setq sql-server
 		 (s-trim-right
 		  (shell-command-to-string "minikube ip")))
 	   (setq sql-port 30432))))
- '(straight-use-package-by-default t))
+ '(straight-use-package-by-default t)
+ '(typescript-indent-level 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
